fb116f51fa29e91af736ecd8a51a2a1a87f12d14
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-19 21:43:23 +0200

    day 2

--- M_5_D_2/Src/Server.js
+++ M_5_D_2/Src/Server.js
@@ -1,3 +1,6 @@
-import express from "express";
-const server = express()
-server.listen (3001, ()=>{ console.log("server running!") })
+import express from 'express';
+const port = 3002;
+const server = express();
+server.listen(port, () => {
+	console.log('server running!', port);
+});
