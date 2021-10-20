2bfc5f634f2b802434b9d979a5b9fe9e4f00f386
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-20 18:11:51 +0200

    day 3

--- M_5_D_3/src/server.js
+++ M_5_D_3/src/server.js
@@ -7,11 +7,8 @@ const server = express();
 
 server.use(express.json());
 server.use('/blogPosts', blogpostRounter);
-
 const port = 3001;
-
 console.table(Endpoints(server));
-
 server.listen(port, () => {
 	console.log('server running-', port);
 });
