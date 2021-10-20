0cdb5395e3397f28dcbe761d3f91ef8ee1c5c3a5
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-21 00:05:17 +0200

    day 2

--- M_5_D_2/Src/Server.js
+++ M_5_D_2/Src/Server.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import Endpoints from 'express-list-endpoints';
-
+import cors from 'cors';
 import studentRounter from './services/students/index.js';
 import booksRounter from './services/books/index.js';
-
+import { errorHandler } from './errorHandler';
+//  errorHandler;
 const server = express();
+
+server.use(cors());
 server.use(express.json());
+
 server.use('/students', studentRounter);
 server.use('/authors', booksRounter);
 
-const port = 3001;
+server.use(errorHandler);
 
+const port = 3001;
 console.table(Endpoints(server));
 
 server.listen(port, () => {
--- M_5_D_2/Src/services/books/index.js
+++ M_5_D_2/Src/services/books/index.js
@@ -8,40 +8,65 @@ const booksRounter = express.Router();
 const currentPath = fileURLToPath(import.meta.url);
 const parentFolderPath = dirname(currentPath);
 const booksJSON = join(parentFolderPath, 'books.json');
-// or  booksJSON= join (dirname(fileURLToPath(import.meta.url)),'books.json')
-booksRounter.post('/', (req, res) => {
-	const allBooks = JSON.parse(fs.readFileSync(booksJSON));
-	const newBook = { ...req.body, createdAt: new Date(), id: uniqid() };
-	allBooks.push(newBook);
-	fs.writeFileSync(booksJSON, JSON.stringify(allBooks));
-	res.status(201).send(`new book is created`);
+// or  booksJSON= join (dirname(fileURLToPath(import.meta.url)),'books.json') 
+booksRounter.post('/', (req, res, next) => {
+	try {
+		const allBooks = JSON.parse(fs.readFileSync(booksJSON));
+		const newBook = { ...req.body, createdAt: new Date(), id: uniqid() };
+		allBooks.push(newBook);
+		fs.writeFileSync(booksJSON, JSON.stringify(allBooks));
+		res.status(201).send(`new book is created`);
+	} catch (error) {
+		next(error);
+	}
 });
 
-booksRounter.get('/', (req, res) => {
-	const allBooks = JSON.parse(fs.readFileSync(booksJSON));
-	res.send(allBooks);
+booksRounter.get('/', (req, res, next) => {
+	try {
+		const allBooks = JSON.parse(fs.readFileSync(booksJSON));
+		if (req.query && req.query.title) {
+			const filter = allBooks.filter((b) => b.title === req.query.title);
+			res.send(filter);
+		} else {
+			res.send(allBooks);
+		}
+	} catch (error) {
+		next(error);
+	}
 });
 
-booksRounter.get('/:id', (req, res) => {
-	const allBooks = JSON.parse(fs.readFileSync(booksJSON));
-	const singleBook = allBooks.find((book) => book.id === req.params.id);
-	res.send(singleBook);
+booksRounter.get('/:id', (req, res, next) => {
+	try {
+		const allBooks = JSON.parse(fs.readFileSync(booksJSON));
+		const singleBook = allBooks.find((book) => book.id === req.params.id);
+		res.send(singleBook);
+	} catch (error) {
+		next(error);
+	}
 });
 
-booksRounter.put('/:id', (req, res) => {
-	const allBooks = JSON.parse(fs.readFileSync(booksJSON));
-	const index = allBooks.findIndex((book) => book.id === req.params.id);
-	const editedBook = { ...allBooks[index], ...req.body };
-	allBooks[index] = editedBook;
-	fs.writeFileSync(booksJSON, JSON.stringify(allBooks));
-	res.send(editedBook);
+booksRounter.put('/:id', (req, res, next) => {
+	try {
+		const allBooks = JSON.parse(fs.readFileSync(booksJSON));
+		const index = allBooks.findIndex((book) => book.id === req.params.id);
+		const editedBook = { ...allBooks[index], ...req.body };
+		allBooks[index] = editedBook;
+		fs.writeFileSync(booksJSON, JSON.stringify(allBooks));
+		res.send(editedBook);
+	} catch (error) {
+		next(error);
+	}
 });
 
-booksRounter.delete('/:id', (req, res) => {
-	const allBooks = JSON.parse(fs.readFileSync(booksJSON));
-	const singleBook = allBooks.filter((book) => book.id !== req.params.id);
-	fs.writeFileSync(booksJSON, JSON.stringify(singleBook));
-	res.status(204).send();
+booksRounter.delete('/:id', (req, res, next) => {
+	try {
+		const allBooks = JSON.parse(fs.readFileSync(booksJSON));
+		const singleBook = allBooks.filter((book) => book.id !== req.params.id);
+		fs.writeFileSync(booksJSON, JSON.stringify(singleBook));
+		res.status(204).send();
+	} catch (error) {
+		next(error);
+	}
 });
 
 export default booksRounter;
