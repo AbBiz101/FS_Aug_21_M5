795ec9437a41001e6387a185cf246c2836add85c
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-10-21 16:29:54 +0200

    day 3-4

--- M_5_D_3_4/FronEnd/blog/src/Ca.jsx
+++ M_5_D_3_4/FronEnd/blog/src/Ca.jsx
@@ -1,21 +1,36 @@
+import { useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
-import cards from './Card';
 
 export default function Ca() {
+	useEffect(() => {
+		const blogFetch = async () => {
+			try {
+				const resp = await fetch('http://localhost:3008/blogPosts');
+				if (resp.ok) {
+					const data = await resp.json();
+					setBlogs(data);
+					setIsLoading(false);
+				} else {
+					console.log('an error happened in the fetch!');
+					setIsLoading(false);
+				}
+			} catch (error) {
+				console.log(error);
+				setIsLoading(false);
+			}
+		};
+		blogFetch();
+	}, []);
 	const [blogs, setBlogs] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
-	const { blogs, isLoading } = cards;
-	return (
-		<div>
-			blogs.map(blg=>
-			<Card key={blg.id}>
-				<Card.Img variant="top" src={blg.cover} />
-				<Card.Body>
-					<Card.Title>{blg.title}</Card.Title>
-					<Card.Text>{blg.category}</Card.Text>
-				</Card.Body>
-			</Card>
-			)
-		</div>
-	);
+
+	return blogs.map((blg) => {
+		<Card key={blogs.id}>
+			<Card.Img variant="top" src={blogs.cover} />
+			<Card.Body>
+				<Card.Title>{blogs.title}</Card.Title>
+				<Card.Text>{blogs.category}</Card.Text>
+			</Card.Body>
+		</Card>;
+	});
 }
